de468917b022834b0b8145b552fe9c635c7afd0c
Anthony Campana <acampana18@gmail.com>
2025-01-12 09:11:09 -0500

    restarted database and updated models. restated database

--- food_blog/blog/models.py
+++ food_blog/blog/models.py
@@ -1,16 +1,8 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-
-class User(models.Model):
-    user_name =models.CharField(max_length=32)
-    password = models.CharField(max_length=20)
-    email =  models.EmailField()
-
-    def __str__(self):
-        return self.user_name 
-
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
