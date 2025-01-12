bee4072aaffe8e777be1a5855c82cf3ce77506ce
Anthony Campana <acampana18@gmail.com>
2025-01-12 09:19:32 -0500

    added logic to authenticate and login users. Also to log user out

--- food_blog/blog/urls.py
+++ food_blog/blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name="register"),
     path("login", views.login, name="login"),
-    path("profile", views.profile, name="profile")
+    path("profile", views.profile, name="profile"),
+    path("logout", views.logout, name='logout'),
 ]
--- food_blog/blog/views.py
+++ food_blog/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import registrationForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout as auth_logout 
+from django.contrib.auth import login as auth_login
 
 def index(request):
     return render(request, "blog/index.html")
@@ -9,7 +15,33 @@ def profile(request):
     return HttpResponse("This is the profile page")
 
 def register(request):
-    return render(request, "blog/register.html")
+
+    if request.method == 'POST':
+        form = registrationForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(form.cleaned_data["userName"], form.cleaned_data["email"], form.cleaned_data["password"])
+            new_user.save()
+            HttpResponseRedirect("/")
+
+    form = registrationForm() 
+    return render(request, "blog/register.html", {"form": form})
 
 def login(request):
-    return render(request, "blog/login.html")
+    print(request.method)
+    if request.method == "POST":
+        password = request.POST["password"]
+        username = request.POST["userName"]
+
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            auth_login(request, user=user)
+            return render(request, "blog/index.html")
+    else:
+        print("inside the login template")
+        form = LoginForm()
+        return render(request, "blog/login.html", {"form": form})
+
+def logout(request):
+    auth_logout(request)
+    return render(request, "blog/index.html")
